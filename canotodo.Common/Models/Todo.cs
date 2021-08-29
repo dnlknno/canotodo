fe767662a98710ec46c333bf72bdd29ceb221e28
dnlknno <danielcardona180478@correo.itm.edu.co>
2021-08-29 11:49:41 -0500

    Add entity

---
+++ canotodo.Common/Models/Todo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace canotodo.Common.Models
+{
+    public class Todo
+    {
+        public DateTime CreateTime { get; set; }
+
+        public string TaskDescription { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
